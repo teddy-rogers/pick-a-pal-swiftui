58d930527422f128fcbef70f1673da717618b6ec
TeddRod <teddy.rogers@outlook.com>
2024-05-11 18:40:20 +0200

    Initial Commit

---
+++ Pick-a-Pal/Pick_a_PalApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pick_a_PalApp.swift
+//  Pick-a-Pal
+//
+//  Created by Teddy on 11/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct Pick_a_PalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

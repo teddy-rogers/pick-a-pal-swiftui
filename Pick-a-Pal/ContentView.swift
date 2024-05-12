1ca7d3b96403536fe55547442c80e93d1ba2d4b1
TeddRod <teddy.rogers@outlook.com>
2024-05-12 14:49:42 +0200

    add learning project

--- Pick-a-Pal/ContentView.swift
+++ Pick-a-Pal/ContentView.swift
@@ -8,12 +8,81 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var names: [String] = []
+    @State private var nameToAdd: String = ""
+    @State private var pickedName: String = ""
+    @State private var shouldRemovePickedName: Bool = false
+    
+    private func pickAName() {
+        if let randomName = names.randomElement() {
+            withAnimation(.linear(duration: 0.1)) {
+                pickedName = randomName
+            }
+            if shouldRemovePickedName {
+                names.removeAll() { name in
+                    return name == randomName
+                }
+            }
+        } else {
+            pickedName = ""
+        }
+    }
+    
+    private func handleSubmit() {
+        if !nameToAdd.isEmpty && !names.contains(nameToAdd) {
+            names.append(nameToAdd)
+            nameToAdd = ""
+        }
+    }
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
+            VStack(spacing: 8) {
+                Image(systemName: "person.3.sequence.fill")
+                    .foregroundStyle(.tint)
+                    .symbolRenderingMode(.hierarchical)
+                Text("Pick-a-Pal")
+                    .font(.title2)
+                    .fontWeight(.semibold)
+            }
+            .font(.title)
+            .bold()
+            Text(!pickedName.isEmpty ? pickedName : " ")
+                .font(.title3)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+                .padding(.top, 8)
+            if !names.isEmpty{
+                HStack {
+                    Spacer()
+                    Button {
+                        names = []
+                    } label: {
+                        Text("Reset")
+                }
+                }
+            }
+            List {
+                ForEach(names, id: \.self) {name in
+                    Text(name)
+                }
+            }
+            .clipShape(RoundedRectangle(cornerRadius: 8))
+            TextField("Add a name", text: $nameToAdd)
+                .autocorrectionDisabled()
+                .onSubmit {
+                    handleSubmit()
+                }
+            Divider()
+            Toggle("Remove when piched", isOn: $shouldRemovePickedName)
+            Button {
+                pickAName()
+            } label: {
+                Text("Pick random name")
+                    .padding(.vertical, 8)
+                    .padding(.horizontal, 16)
+            }
+            .buttonStyle(.borderedProminent)
+            .font(.title2)
         }
         .padding()
     }
